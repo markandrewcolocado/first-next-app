e33b9ccb2490cf7136e40be1e40e159aaf7cdb2e
Mark Andrew Colocado <mark05@simplexi.com>
2023-03-29 10:35:03 +0800

    Navigating programmatically, example: changing page after submitting a form.

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
 function CLientProjectsPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/projecta");
+    // alternative
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>The Project of a Given Client.</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
