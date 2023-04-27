e46b4a3553fb462f94af7a453539bfe405ac778a
Mark Andrew Colocado <mark05@simplexi.com>
2023-04-27 13:33:04 +0800

    working with _app.js to apply metadata to all components.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        {/* Meta tag often added to pages to ensure that the page is responsive and scales correctly */}
+        {/* You can put meta data in here to apply it to all the pages within the app */}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
