3ea3e10ef55f27227b9b3eaab7cad5cc8ee5afd4
Mark Andrew Colocado <mark05@simplexi.com>
2023-05-04 14:29:53 +0800

    setting up dynamic API route and sending requests to dynamic API route.

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,29 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedbackData } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{`${item.text} - ${item.email}`}</li>
-      ))}
-    </ul>
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {`${item.text}`}{" "}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
   // return <h1>test</h1>;
 }
@@ -16,7 +33,6 @@ export async function getStaticProps() {
   // You can instead use the nodejs code to directly fetch the data since this code will only run in the server and not in the client
   const filePath = buildFeedbackPath();
   const data = extractFeedbackData(filePath);
-  console.log(data);
   return {
     props: {
       feedbackItems: data,
