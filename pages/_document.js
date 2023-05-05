f62a9a71da24436c0fba4cbe131cf692f2709560
Mark Andrew Colocado <mark05@simplexi.com>
2023-05-05 10:45:57 +0800

    Sample validation using react-hook-form and yup to build schema.

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-css-tags */
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,151 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FormValidationSchema } from "../schema/validation";
+
+function Home() {
+  // form validation rules
+  const validationSchema = FormValidationSchema();
+  const formOptions = { resolver: yupResolver(validationSchema) };
+
+  // get functions to build form with useForm() hook
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    // display form data on success
+    alert("SUCCESS!! :-)\n\n" + JSON.stringify(data, null, 4));
+    return false;
+  }
+
+  return (
+    <div className="card m-3">
+      <h5 className="card-header">Next.js - Form Validation Example</h5>
+      <div className="card-body">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-row">
+            <div className="form-group col">
+              <label>Title</label>
+              <select
+                name="title"
+                {...register("title")}
+                className={`form-control ${errors.title ? "is-invalid" : ""}`}>
+                <option value=""></option>
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Miss">Miss</option>
+                <option value="Ms">Ms</option>
+              </select>
+              <div className="invalid-feedback">{errors.title?.message}</div>
+            </div>
+            <div className="form-group col-5">
+              <label>First Name</label>
+              <input
+                name="firstName"
+                type="text"
+                {...register("firstName")}
+                className={`form-control ${
+                  errors.firstName ? "is-invalid" : ""
+                }`}
+              />
+              <div className="invalid-feedback">
+                {errors.firstName?.message}
+              </div>
+            </div>
+            <div className="form-group col-5">
+              <label>Last Name</label>
+              <input
+                name="lastName"
+                type="text"
+                {...register("lastName")}
+                className={`form-control ${
+                  errors.lastName ? "is-invalid" : ""
+                }`}
+              />
+              <div className="invalid-feedback">{errors.lastName?.message}</div>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col">
+              <label>Date of Birth</label>
+              <input
+                name="dob"
+                type="date"
+                {...register("dob")}
+                className={`form-control ${errors.dob ? "is-invalid" : ""}`}
+              />
+              <div className="invalid-feedback">{errors.dob?.message}</div>
+            </div>
+            <div className="form-group col">
+              <label>Email</label>
+              <input
+                name="email"
+                type="text"
+                {...register("email")}
+                className={`form-control ${errors.email ? "is-invalid" : ""}`}
+              />
+              <div className="invalid-feedback">{errors.email?.message}</div>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col">
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                {...register("password")}
+                className={`form-control ${
+                  errors.password ? "is-invalid" : ""
+                }`}
+              />
+              <div className="invalid-feedback">{errors.password?.message}</div>
+            </div>
+            <div className="form-group col">
+              <label>Confirm Password</label>
+              <input
+                name="confirmPassword"
+                type="password"
+                {...register("confirmPassword")}
+                className={`form-control ${
+                  errors.confirmPassword ? "is-invalid" : ""
+                }`}
+              />
+              <div className="invalid-feedback">
+                {errors.confirmPassword?.message}
+              </div>
+            </div>
+          </div>
+          <div className="form-group form-check">
+            <input
+              name="acceptTerms"
+              type="checkbox"
+              {...register("acceptTerms")}
+              id="acceptTerms"
+              className={`form-check-input ${
+                errors.acceptTerms ? "is-invalid" : ""
+              }`}
+            />
+            <label htmlFor="acceptTerms" className="form-check-label">
+              Accept Terms & Conditions
+            </label>
+            <div className="invalid-feedback">
+              {errors.acceptTerms?.message}
+            </div>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary mr-1">
+              Register
+            </button>
+            <button
+              type="button"
+              onClick={() => reset()}
+              className="btn btn-secondary">
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
