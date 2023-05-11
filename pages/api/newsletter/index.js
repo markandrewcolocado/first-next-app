7370aa102b5912674da8bf8959b93866eac2bec0
Mark Andrew Colocado <mark05@simplexi.com>
2023-05-11 08:30:04 +0800

    Saving data to mongodb.

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,11 +1,28 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
-    const email = req.body.email;
-    if (!email || !email.includes("@")) {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address!" });
       return;
     }
-    res.status(201).json({ message: "Success!", email });
+
+    const client = await MongoClient.connect(
+      // newsletter will be the name of the database
+      "mongodb+srv://mark05:A6815869193@project1.sbdoj8d.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    // emails is the name of the collection
+    const response = await db
+      .collection("emails")
+      .insertOne({ email: userEmail });
+    console.log(response);
+
+    client.close();
+
+    res.status(201).json({ message: "Success!", email: userEmail });
   }
 }
 
